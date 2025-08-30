e849b833710edf6cf0d3752d6ad11b102656c593
FABIO-Eng.Software <FABIO-Eng.Software@DESKTOP-3GDMBEN>
2025-08-30 20:19:35 -0300

    Adicionar arquivos de projeto.

---
+++ Entrada_de_dados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada_de_dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Olá  " + nome);
+            Console.ReadKey();
+        }
+    }
+}
